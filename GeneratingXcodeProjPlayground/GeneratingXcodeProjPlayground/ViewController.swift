07cb8400a06870eb141d1bfe30b109b4ea129373
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2019-10-30 14:25:20 +0100

    Add Core framework

--- GeneratingXcodeProjPlayground/GeneratingXcodeProjPlayground/ViewController.swift
+++ GeneratingXcodeProjPlayground/GeneratingXcodeProjPlayground/ViewController.swift
@@ -1,20 +1,12 @@
-//
-//  ViewController.swift
-//  GeneratingXcodeProjPlayground
-//
-//  Created by Andrea Cipriani on 30.10.19.
-//  Copyright © 2019 Andrea Cipriani. All rights reserved.
-//
-
 import UIKit
+import Core
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let networkClient = NetworkClient()
+        print(networkClient.makeNetworkRequest())
     }
-
-
 }
 
