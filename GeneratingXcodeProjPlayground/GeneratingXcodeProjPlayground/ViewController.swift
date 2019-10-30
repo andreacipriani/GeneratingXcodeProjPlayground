1a4b511b49f92a001d601b7bf0322a006d979190
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2019-10-30 14:17:49 +0100

    Create single View Xcode project

---
+++ GeneratingXcodeProjPlayground/GeneratingXcodeProjPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeneratingXcodeProjPlayground
+//
+//  Created by Andrea Cipriani on 30.10.19.
+//  Copyright © 2019 Andrea Cipriani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
